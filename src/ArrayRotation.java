fc11a07c31617dc853495ce60a4bb322a1ba4e2f
Mannuru <arunm057@gmail.com>
2017-07-30 15:52:48 -0400

    rotation

--- src/ArrayRotation.java
+++ src/ArrayRotation.java
@@ -13,33 +13,25 @@ public class ArrayRotation {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		int[] arrElements = {1,2,3,4,5,6,7};
-		leftRotate(arrElements, 2, 7);
-        printArray(arrElements,7);
+		rotateArray(arrElements,2);
 	}
-	/*Function to left rotate arr[] of size n by d*/
-    static void leftRotate(int arr[], int d, int n) 
-    {
-        int i;
-        for (i = 0; i < d; i++)
-            leftRotatebyOne(arr, n);
-    }
- 
-   static void leftRotatebyOne(int arr[], int n) 
-    {
-        int i, temp;
-        temp = arr[0];
-        for (i = 0; i < n - 1; i++)
-            arr[i] = arr[i + 1];
-        arr[i] = temp;
-    }
- 
-    /* utility function to print an array */
-   static void printArray(int arr[], int size) 
-    {
-        int i;
-        for (i = 0; i < size; i++)
-            System.out.print(arr[i] + " ");
-    }
+	static void rotateArray(int[] arrElements,int d){
+		
+		int len = arrElements.length;
+		int[] finalArray = new int[len];
+		int k=0;
+		for(int j=d; j < len; j++){
+			finalArray[k] = arrElements[j];
+			k++;
+		}
+		int m=0;
+		for(int l=len-d; l < len; l++){
+			finalArray[l] = arrElements[m];
+			m++;
+		}
+		for(int x: finalArray){
+			System.out.println(x);
+		}
+	}
 }
