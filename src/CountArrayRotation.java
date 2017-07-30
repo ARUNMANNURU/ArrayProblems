86b644cc91ce1fe08b787f3652502aa1b63f06c8
Mannuru <arunm057@gmail.com>
2017-07-30 19:31:19 -0400

    count rotation

---
+++ src/CountArrayRotation.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+
+/**
+ * @author ArunMannuru
+ *
+ */
+public class CountArrayRotation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arrEle = {5,6,1,2,3,4};
+		int low =0;
+		int high = arrEle.length-1;
+		System.out.println(countRotations(arrEle,low,high));
+	}
+	// Returns count of rotations for an array which
+	// is first sorted in ascending order, then rotated
+	static int countRotations(int arr[], int low, int high)
+	{
+	    // This condition is needed to handle the case
+	    // when array is not rotated at all
+	    if (high < low)
+	        return 0;
+	 
+	    // If there is only one element left
+	    if (high == low)
+	        return low;
+	 
+	    // Find mid
+	    int mid = low + (high - low)/2; /*(low + high)/2;*/
+	 
+	    // Check if element (mid+1) is minimum element.
+	    // Consider the cases like {3, 4, 5, 1, 2}
+	    if (mid < high && arr[mid+1] < arr[mid])
+	       return (mid+1);
+	 
+	    // Check if mid itself is minimum element
+	    if (mid > low && arr[mid] < arr[mid - 1])
+	       return mid;
+	 
+	    // Decide whether we need to go to left half or
+	    // right half
+	    if (arr[high] > arr[mid])
+	       return countRotations(arr, low, mid-1);
+	 
+	    return countRotations(arr, mid+1, high);
+	}
+}
